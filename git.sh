4aef9db53285482e11ccfbb774a32af49c8abb52
沈唁 <52o@qq52o.cn>
2019-03-11 11:53:36 +0800

    Create git.sh

---
+++ git.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd $1;
+
+case $2 in
+    swoole)
+        echo "This is the git used at work.";
+        git config user.name ""
+        git config user.email ""
+        ssh-keygen -t rsa -C ""
+        ;;
+    github)
+        echo "This is the git used at github.";
+        git config user.name ""
+        git config user.email ""
+        ;;
+    unset-all)
+        # 删除全局
+        git config --global --unset user.name
+        git config --global --unset user.email
+        ;;
+    unset)
+        # 删除指定目录下
+        git config --unset user.name
+        git config --unset user.email
+        ;;
+    *)
+        echo "Usage: $0 yourpath {swoole|github|unset-all|unset}"
+        exit;
+        ;;
+esac
+
+echo "Done!"
